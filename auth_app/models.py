d170a76da3a864b4528e487fae4340f5bb2766bc
SandorBalazsi <166751447+SandorBalazsi@users.noreply.github.com>
2025-10-04 16:48:58 +0200

    made first modules and migrated

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,10 @@
+
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email =  models.CharField(max_length=250)
+    fullname = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.fullname
+
--- boards_app/models.py
+++ boards_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from auth_app import models as Auth_User 
 
 # Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=250)
+    owner = models.ForeignKey(Auth_User.User,on_delete=models.CASCADE, related_name='owned_boards')
+    members = models.ManyToManyField(Auth_User.User, related_name='boards', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
