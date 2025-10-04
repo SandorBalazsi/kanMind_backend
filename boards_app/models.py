6f7f2906683ea41a70c0b8f132f374c074b367d6
SandorBalazsi <166751447+SandorBalazsi@users.noreply.github.com>
2025-10-04 17:32:50 +0200

    made first modules and migrated

--- boards_app/models.py
+++ boards_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from auth_app import models as Auth_User 
 
 # Create your models here.
@@ -11,3 +12,51 @@ class Board(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('to-do','To Do'),
+        ('in-progress','In Progress'),
+        ('review','Review'),
+        ('done','Done'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='to-do')
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium')
+    assignee = models.ForeignKey(Auth_User.User, on_delete=models.CASCADE,null=True,blank=True,related_name='assigned_tasks')
+    reviewer = models.ForeignKey(Auth_User.User, on_delete=models.CASCADE,null=True,blank=True,related_name='review_tasks')
+    due_date = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        Auth_User.User,
+        on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f"Comment by {self.author.fullname} on {self.task.title}"
